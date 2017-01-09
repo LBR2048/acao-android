be0230e94f320ef30e866b8c1dcc02ea8579826c
LBR2048 <leo.ardjomand@gmail.com>
2017-01-09 17:56:24 -0200

    Do not hide keyboard when sending messages Remove toast when message is sent

--- app/src/main/java/com/penseapp/acaocontabilidade/chat/view/MessagesActivity.java
+++ app/src/main/java/com/penseapp/acaocontabilidade/chat/view/MessagesActivity.java
@@ -9,7 +9,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.penseapp.acaocontabilidade.R;
 import com.penseapp.acaocontabilidade.chat.adapters.MessagesAdapter;
@@ -17,7 +16,6 @@ import com.penseapp.acaocontabilidade.chat.model.Message;
 import com.penseapp.acaocontabilidade.chat.presenter.MessagesPresenter;
 import com.penseapp.acaocontabilidade.chat.presenter.MessagesPresenterImpl;
 import com.penseapp.acaocontabilidade.domain.FirebaseHelper;
-import com.penseapp.acaocontabilidade.domain.Utilities;
 
 import java.util.ArrayList;
 
@@ -82,6 +80,19 @@ public class MessagesActivity extends AppCompatActivity implements MessagesView 
         messagesAdapter = new MessagesAdapter(mMessages);
         mMessagesRecyclerView.setAdapter(messagesAdapter);
         mMessagesRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        mMessagesRecyclerView.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+//            @Override
+//            public void onLayoutChange(View view, int i, int i1, int i2, int i3, int i4, int i5, int i6, int i7) {
+//                mMessagesRecyclerView.postDelayed(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        mMessagesRecyclerView.smoothScrollToPosition(messagesAdapter.getItemCount() - 1);
+//                    }
+//                }, 100);
+//                mMessagesRecyclerView.smoothScrollToPosition(mMessages.size() - 1);
+//                Toast.makeText(getApplicationContext(), "Evento", Toast.LENGTH_SHORT).show();
+//            }
+//        });
     }
 
     private void clearRecyclerView() {
@@ -98,7 +109,7 @@ public class MessagesActivity extends AppCompatActivity implements MessagesView 
     }
 
     public void onClickSendMessage(View view) {
-        Toast.makeText(getApplicationContext(), "Enviando mensagem", Toast.LENGTH_SHORT).show();
+//        Toast.makeText(getApplicationContext(), "Enviando mensagem", Toast.LENGTH_SHORT).show();
         EditText messageEdit = (EditText) findViewById(R.id.messageEdit);
         Button sendButton = (Button) findViewById(R.id.sendButton);
 
@@ -109,7 +120,7 @@ public class MessagesActivity extends AppCompatActivity implements MessagesView 
 
         // Clear messageEdit and hide keyboard
         messageEdit.getText().clear();
-        Utilities.hideSoftKeyboard(this);
+//        Utilities.hideSoftKeyboard(this);
 
         if (!text.isEmpty()) {
             messagesPresenter.sendMessage(text, senderId, senderName);
