16b5042211dd87f547ad55f57340da9f8f24d4cc
LBR2048 <leo.ardjomand@gmail.com>
2017-01-30 17:02:06 -0200

    Fix notifications menu not showing

--- app/src/main/java/com/penseapp/acaocontabilidade/news/view/NewsFragment.java
+++ app/src/main/java/com/penseapp/acaocontabilidade/news/view/NewsFragment.java
@@ -65,7 +65,7 @@ public class NewsFragment extends Fragment implements NewsView {
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        super.onCreateOptionsMenu(menu, inflater);
+//        super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.menu_news, menu);
     }
 
@@ -91,6 +91,7 @@ public class NewsFragment extends Fragment implements NewsView {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_news, container, false);
     }
