400b735f7649cf6939f0c3ec9c9d9c3a1a66263a
LBR2048 <leo.ardjomand@gmail.com>
2017-01-30 16:48:14 -0200

    Remove Reminders tab

--- app/src/main/java/com/penseapp/acaocontabilidade/chat/view/TabbedMainActivity.java
+++ app/src/main/java/com/penseapp/acaocontabilidade/chat/view/TabbedMainActivity.java
@@ -173,7 +173,7 @@ public class TabbedMainActivity extends AppCompatActivity implements
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
+            // Show 2 total pages.
             return 2;
         }
 
