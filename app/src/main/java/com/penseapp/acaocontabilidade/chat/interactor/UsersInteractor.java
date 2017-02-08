9d9900559be822dc034012da87aef2bc402d64a9
LBR2048 <leo.ardjomand@gmail.com>
2017-02-08 18:12:34 -0200

    Add save user preferences on shared preferences using UsersView, UsersPresenter and UsersInteractor

---
+++ app/src/main/java/com/penseapp/acaocontabilidade/chat/interactor/UsersInteractor.java
@@ -0,0 +1,9 @@
+package com.penseapp.acaocontabilidade.chat.interactor;
+
+/**
+ * Created by unity on 08/02/17.
+ */
+
+public interface UsersInteractor {
+    void getCurrentUserDetails();
+}
---
+++ app/src/main/java/com/penseapp/acaocontabilidade/chat/presenter/UsersPresenterImpl.java
@@ -0,0 +1,31 @@
+package com.penseapp.acaocontabilidade.chat.presenter;
+
+import com.penseapp.acaocontabilidade.chat.interactor.UsersInteractor;
+import com.penseapp.acaocontabilidade.chat.interactor.UsersInteractorImpl;
+import com.penseapp.acaocontabilidade.chat.view.UsersView;
+import com.penseapp.acaocontabilidade.login.model.User;
+
+/**
+ * Created by unity on 08/02/17.
+ */
+
+public class UsersPresenterImpl implements UsersPresenter {
+
+    private UsersView usersView;
+    private UsersInteractor usersInteractor;
+
+    public UsersPresenterImpl(UsersView usersView) {
+        this.usersView = usersView;
+        this.usersInteractor = new UsersInteractorImpl(this);
+    }
+
+    @Override
+    public void getCurrentUserDetails() {
+        usersInteractor.getCurrentUserDetails();
+    }
+
+    @Override
+    public void onReceiveCurrentUserDetails(User user) {
+        usersView.onReceiveCurrentUserDetails(user);
+    }
+}
