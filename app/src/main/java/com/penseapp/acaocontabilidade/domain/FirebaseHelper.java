25c8c3c4cdc4cfa8f73615efa5ad72ca2e891fc2
LBR2048 <leo.ardjomand@gmail.com>
2017-03-02 17:55:10 -0300

    Replace chats node with chat-messages

--- app/src/main/java/com/penseapp/acaocontabilidade/domain/FirebaseHelper.java
+++ app/src/main/java/com/penseapp/acaocontabilidade/domain/FirebaseHelper.java
@@ -10,17 +10,19 @@ import com.google.firebase.database.FirebaseDatabase;
  */
 public class FirebaseHelper {
     private static final String LOG_TAG = FirebaseHelper.class.getSimpleName();
+    public static final String USER_CHATS_PROPERTIES_PATH = "user-chats-properties";
 
     // Firebase database
     private FirebaseDatabase database;
     private DatabaseReference databaseRef;
     private static final String USERS_PATH = "users";
-    private static final String CHATS_PATH = "chats";
+    private static final String CHAT_MESSAGES_PATH = "chat-messages";
     private static final String USER_CHATS_PATH = "user-chats";
-    private static final String CHAT_USERS_PATH = "chat-users";
-    private static final String USER_CHATCONTACTS_CHAT = "user-chatContacts:chat";
-    private static final String MESSAGES_PATH = "messages";
+    private static final String USER_CHAT_CONTACTS_CHAT = "user-chatContacts:chat";
     private static final String NEWS_PATH = "news";
+
+    // TODO útil apenas para grupos retirar por enquanto
+//    private static final String CHAT_USERS_PATH = "chat-users";
 
     private static class SingletonHolder {
         private static final FirebaseHelper INSTANCE = new FirebaseHelper();
@@ -34,7 +36,7 @@ public class FirebaseHelper {
     private FirebaseHelper(){
         if (database == null) {
             database = FirebaseDatabase.getInstance();
-            database.setPersistenceEnabled(true);
+//            database.setPersistenceEnabled(true);
             databaseRef = database.getReference();
         }
     }
@@ -67,12 +69,8 @@ public class FirebaseHelper {
         return databaseRef.child(USERS_PATH);
     }
 
-    public DatabaseReference getChatsReference(){
-        return databaseRef.child(CHATS_PATH);
-    }
-
-    public DatabaseReference getCurrentChatReference(String chatId){
-        return getChatsReference().child(chatId).child(MESSAGES_PATH);
+    public DatabaseReference getChatMessagesReference(){
+        return databaseRef.child(CHAT_MESSAGES_PATH);
     }
 
     public DatabaseReference getUserChatsReference(){
@@ -84,36 +82,25 @@ public class FirebaseHelper {
     }
 
     public DatabaseReference getUserChatContactsReference(){
-        return databaseRef.child(USER_CHATCONTACTS_CHAT);
+        return databaseRef.child(USER_CHAT_CONTACTS_CHAT);
     }
 
-    public DatabaseReference getChatUsersReference(){
-        return databaseRef.child(CHAT_USERS_PATH);
+    public DatabaseReference getUserChatPropertiesReference(){
+        return databaseRef.child(USER_CHATS_PROPERTIES_PATH);
     }
 
-    public DatabaseReference getCurrentChatUsersReference(){
-        return getChatUsersReference().child(getAuthUserId());
-    }
+    // TODO útil apenas para grupos retirar por enquanto
+//    public DatabaseReference getChatUsersReference(){
+//        return databaseRef.child(CHAT_USERS_PATH);
+//    }
+
+    // TODO útil apenas para grupos retirar por enquanto
+//    public DatabaseReference getCurrentChatUsersReference(){
+//        return getChatUsersReference().child(getAuthUserId());
+//    }
 
     public DatabaseReference getNewsReference(){
         return databaseRef.child(NEWS_PATH);
     }
 
-
-//    public String getCurrentUserType() {
-//        String type;
-//        getCurrentUserReference().child("type").addListenerForSingleValueEvent(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                type = dataSnapshot.getValue().toString();
-//                System.out.println(type);
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//
-//            }
-//        });
-//        return type;
-//    }
 }
