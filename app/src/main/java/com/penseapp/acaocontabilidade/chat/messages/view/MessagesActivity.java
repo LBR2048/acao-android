22b53f39a377590dedbd11df8f4e39d006a5a90c
LBR2048 <leo.ardjomand@gmail.com>
2018-01-30 20:54:23 -0200

    Update image picker to use Storage Access Framework

--- app/src/main/java/com/penseapp/acaocontabilidade/chat/messages/view/MessagesActivity.java
+++ app/src/main/java/com/penseapp/acaocontabilidade/chat/messages/view/MessagesActivity.java
@@ -168,9 +168,19 @@ public class MessagesActivity extends AppCompatActivity implements MessagesView 
     }
 
     private void pickPhotoFromGallery() {
-        // Create intent for picking a photo from the gallery
-        Intent intent = new Intent(Intent.ACTION_PICK,
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        // ACTION_OPEN_DOCUMENT is the intent to choose a file via the system's file
+        // browser.
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+
+        // Filter to only show results that can be "opened", such as a
+        // file (as opposed to a list of contacts or timezones)
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        // Filter to show only images, using the image MIME data type.
+        // If one wanted to search for ogg vorbis files, the type would be "audio/ogg".
+        // To search for all documents available via installed storage providers,
+        // it would be "*/*".
+        intent.setType("image/*");
 
         // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
         // So as long as the result is not null, it's safe to use the intent.
