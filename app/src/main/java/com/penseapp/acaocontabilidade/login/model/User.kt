303385c47b8cc79034be18e78ac7a22638fe19d3
LBR2048 <leo.ardjomand@gmail.com>
2018-08-31 15:25:37 +0200

    Convert User data class into common class
    Data classes are not supported by Firebase

--- app/src/main/java/com/penseapp/acaocontabilidade/login/model/User.kt
+++ app/src/main/java/com/penseapp/acaocontabilidade/login/model/User.kt
@@ -1,14 +1,11 @@
 package com.penseapp.acaocontabilidade.login.model
 
-/**
- * Created by unity on 08/08/16.
- */
-data class User (
-    var key: String,
-    var name: String = "name",
-    var email: String = "email@email.com",
-    var company: String = "company",
-    var type: String = "type"
+class User (
+        var key: String,
+        var name: String = "name",
+        var email: String = "email@email.com",
+        var company: String = "company",
+        var type: String = "type"
 ) {
     override fun toString(): String {
         return name
