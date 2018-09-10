73008622a3c86d29fe62b8bb6a2168164fd6096c
LBR2048 <leo.ardjomand@gmail.com>
2018-09-10 12:52:15 +0200

    Show contacts on main screen

--- app/src/main/java/com/penseapp/acaocontabilidade/MainActivity.kt
+++ app/src/main/java/com/penseapp/acaocontabilidade/MainActivity.kt
@@ -40,8 +40,8 @@ class MainActivity : AppCompatActivity(),
         if (savedInstanceState == null) {
             supportFragmentManager
                     .beginTransaction()
-                    .replace(R.id.fragmentHolder, ChatsFragment.newInstance(this))
-//                    .replace(R.id.fragmentHolder, ContactsFragment.newInstance())
+//                    .replace(R.id.fragmentHolder, ChatsFragment.newInstance(this))
+                    .replace(R.id.fragmentHolder, ContactsFragment.newInstance())
                     .commit()
         }
 
